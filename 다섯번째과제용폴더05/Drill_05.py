413103b9066e2cd9236d5b00e77055789d77a6ae
rexsot <112843175+rexsot@users.noreply.github.com>
2022-09-19 20:56:35 +0900

    drill 05 완성
    2021182023 오서진

---
+++ 다섯번째과제용폴더05/Drill_05.py
@@ -0,0 +1,35 @@
+import turtle
+import random
+
+turtle.shape('turtle')
+
+def w_move():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def a_move():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def s_move():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+def d_move():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+def restart():
+    turtle.reset()
+
+
+turtle.onkey(w_move, 'w')
+turtle.onkey(a_move, 'a')
+turtle.onkey(s_move, 's')
+turtle.onkey(d_move, 'd')
+turtle.onkey(restart, 'Escape')
+turtle.listen()
