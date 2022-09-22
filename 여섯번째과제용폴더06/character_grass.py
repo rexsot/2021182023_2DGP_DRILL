95ec8290092f8bca1adf5ec4d143a137e6174ed8
LJaeMoon <100143631+wellcometo@users.noreply.github.com>
2022-09-22 15:24:34 +0900

    Drill 06 완성
    2021182023 오서진

---
+++ 여섯번째과제용폴더06/character_grass.py
@@ -0,0 +1,79 @@
+from pico2d import *
+import math
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+x = 400
+y = 90
+r = 235
+
+
+
+def moverec():
+    global x
+    global y
+    while(x < 780):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x += 2
+        delay(0.01)
+    while(y < 560):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y += 2
+        delay(0.01)
+    while(x > 20):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x -= 2
+        delay(0.01)
+    while(y > 90):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y -= 2
+        delay(0.01)
+    while(x < 400):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x += 2
+        delay(0.01)
+
+# 원점 좌표 (400 ,325) 
+
+def movecir():
+    global x
+    global y
+    r = 235
+    c = 90 #각도 (270 -> 360(0) -> 270
+    while(c < 360):
+        x = 400 - r * math.cos(c / 360 * 2 * math.pi)
+        y = 325 - r * math.sin(c / 360 * 2 * math.pi)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        c += 0.1
+        
+    c = 0
+    
+    while(c < 90):
+        x = 400 - r * math.cos(c / 360 * 2 * math.pi)
+        y = 325 - r * math.sin(c / 360 * 2 * math.pi)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        c += 0.1
+    
+    
+
+#moverec()
+movecir()
+
+close_canvas()
